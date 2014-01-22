0963925df4caf017c315ef616f5be4e17b37907e
juscelinotanaka <juscelino.tanaka@gmail.com>
2014-01-22 18:52:34 -0400

    Initial commit from local git

---
+++ main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  TP2
+//
+//  Created by Juscelino Tanaka on 22/01/14.
+//  Copyright (c) 2014 Juscelino Tanaka. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
